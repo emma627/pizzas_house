444b3c1a1af7b0f03dc1524bd612cc59b6d23a26
jinyan YANG <jinyan.yang627@gmail.com>
2021-05-06 10:37:55 +0200

    db

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
+
+<?php 
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
 <?php  include ("templates/header.php")  ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+<?php foreac   ?>
+
+
+
+		
+	</div>
+</div>
+
+
+
+
 <?php  include ("templates/footer.php")  ?>
 
 
